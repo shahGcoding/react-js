568d0e851831902d08db643a05f01498bf48b11f
shahzaib <shahzaibshahg90@example.com>
2024-12-23 20:15:41 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  //let counter = 10;
+  let [counter, setCounter] = useState(0)
+
+  const addValue = () =>{
+      if(counter < 20){
+        counter = counter + 1;
+        setCounter(counter);
+      }
+    }
+     
+ 
+
+  const removValue = () =>{
+    if(counter > 0){
+    counter = counter - 1;
+    setCounter(counter);
+  }
+}
+
+  return (
+    <>
+      <h1>hello react</h1>
+      <h2>counter value: {counter} </h2>
+
+      <button
+      onClick={addValue}
+      >Add value</button>
+      <br />
+      <button
+      onClick={removValue}
+      >remove value</button>
+    </>
+  )
+}
+
+export default App
